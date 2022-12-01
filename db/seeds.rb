3cbec448056363caf2d0c1db0780a59e69b2e48a
Craig <craig.coleman.cc@gmail.com>
2022-12-01 14:23:38 -0600

    Seed data in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+puts "Starting seed..."
+user = User.create(username: "craig.coleman", first_name: "Craig", last_name:"Coleman", email:"craig.coleman@teacher.com")
+course = user.courses.create(title: "Biology", period: 1, grade_level: 9)
+student = Student.create(first_name: "John", last_name: "Smith", grade_level: 9, classification: "freshman", birth_date: 20081119)
+period = course.periods.create(number: course.period, start_time: '08:50', end_time: '08:50', student_id: student.id)
+assignment = student.assignments.create(title: "Chapter 1 Homework", description: "Page 59 Questions 1-15", assign_date: 20220820, due_date: 20220821, score: 100, course_id: course.id)
+puts "Seed Completed!"
