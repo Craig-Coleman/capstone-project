6105f65cd94a9533710625670b215f7fe74e3144
Craig <craig.coleman.cc@gmail.com>
2022-12-01 14:12:18 -0600

    Exceptions for unnecessary routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  resources :assignments
-  resources :students
-  resources :periods
-  resources :courses
-  resources :users
+  resources :assignments, except: [:new, :edit]
+  resources :students, except: [:new, :edit]
+  resources :periods, except: [:new, :edit]
+  resources :courses, except: [:new, :edit]
+  resources :users, only: [:show, :create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
