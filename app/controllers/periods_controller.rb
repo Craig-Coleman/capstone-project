7ec3956ac3ddae54e1a09d4f7463abca6508660d
Craig <craig.coleman.cc@gmail.com>
2022-12-01 14:14:40 -0600

    Periods controller methods

--- app/controllers/periods_controller.rb
+++ app/controllers/periods_controller.rb
@@ -1,2 +1,36 @@
 class PeriodsController < ApplicationController
+
+    def index
+        periods = Period.all 
+        render json: periods, status: :ok 
+    end
+
+    def create 
+        period = Period.create!(period_params)
+        render json: period, status: :created 
+    end
+
+    def show
+        period = Period.find(params[:id])
+        render json: period, status: :ok 
+    end
+
+    def update 
+        period = Period.find(params[:id])
+        period.update!(period_params)
+        render json: period, status: :ok
+    end
+
+    def destroy 
+        period = Period.find(params[:id])
+        period.destroy
+        head :no_content 
+    end
+
+    private
+
+    def period_params 
+        params.permit(:id, :number, :start_time, :end_time, :course_id, :student_id)
+    end
+    
 end
