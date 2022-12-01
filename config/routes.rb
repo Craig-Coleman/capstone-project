6e0e45522dc3223929aa9c1dbe9d7953653ba11d
Craig <craig.coleman.cc@gmail.com>
2022-12-01 14:08:58 -0600

    Resources created and migrated

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resources :assignments
+  resources :students
+  resources :periods
+  resources :courses
+  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
