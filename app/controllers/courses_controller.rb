89c5bd6d65ff4fa8514629cad0b27605fdac2d39
Craig <craig.coleman.cc@gmail.com>
2022-12-01 14:14:04 -0600

    Courses controller methods

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,2 +1,36 @@
 class CoursesController < ApplicationController
+
+    def index 
+        courses = Course.all 
+        render json: courses, status: :ok
+    end
+
+    def create 
+        course = Course.create!(course_params)
+        render json: course, status: :created 
+    end
+
+    def show 
+        course = Course.find(params[:id])
+        render json: course, status: :ok 
+    end
+
+    def update
+        course = Course.find(params[:id])
+        course.update!(course_params)
+        render json: course, status: :ok 
+    end
+
+    def destroy 
+        course = Course.find(params[:id])
+        course.destroy 
+        head :no_content 
+    end
+
+    private 
+
+    def course_params 
+        params.permit(:id, :title, :period, :grade_level, :user_id)
+    end
+    
 end
