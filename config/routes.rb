c8ee37c76bb074522e859f668edcc9264b25a46d
Craig <craig.coleman.cc@gmail.com>
2022-12-01 14:18:05 -0600

    Custom routes in route file for session handling

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ Rails.application.routes.draw do
   resources :periods, except: [:new, :edit]
   resources :courses, except: [:new, :edit]
   resources :users, only: [:show, :create]
+
+  get '/me', to: "users#show"
+  post '/login', to: "sessions#create"
+  delete '/logout', to: "sessions#destroy"
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
