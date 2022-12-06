0f1497b344dd1d0b56e11b669a8a19d89482c9a8
Craig <craig.coleman.cc@gmail.com>
2022-12-06 12:23:13 -0600

    Validations in period model

--- app/models/period.rb
+++ app/models/period.rb
@@ -1,4 +1,8 @@
 class Period < ApplicationRecord
     belongs_to :student 
     belongs_to :course 
+
+    validates :number, presence: true, numericality: { in: 1..8, only_integer: true}
+    validates :start_time, presence: true 
+    validates :end_time, presence: true
 end
