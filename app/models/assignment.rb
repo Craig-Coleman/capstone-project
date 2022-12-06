21fab2c96f659da59b4b76c78e6707b0ca7c7177
Craig <craig.coleman.cc@gmail.com>
2022-12-06 12:26:56 -0600

    Validations in assignment model

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,4 +1,9 @@
 class Assignment < ApplicationRecord
     belongs_to :course 
     belongs_to :student 
+
+    validates :title, presence: true
+    validates :assign_date, presence: true 
+    validates :due_date, presence: true 
+    validates :score, presence: true, numericality: { greater_than_or_equal_to: 0 }
 end
