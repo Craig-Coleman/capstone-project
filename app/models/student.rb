541c0753057af293a80647ec7fb5cb161f04fcbf
Craig <craig.coleman.cc@gmail.com>
2022-12-06 12:21:00 -0600

    Validations in student model

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,11 @@ class Student < ApplicationRecord
     has_many :periods 
     has_many :assignments, dependent: :destroy  
     has_many :courses, through: :periods 
+
+    validates :first_name, presence: true 
+    validates :last_name, presence: true 
+    validates :grade_level, presence: true, numericality: { in: 7..12, only_integer: true  }
+    validates :classification, presence: true 
+    validates :birth_date, presence: true 
+
 end
