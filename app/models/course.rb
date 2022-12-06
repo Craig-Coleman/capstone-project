8d768318651470d1606dd4840a550135ef0b51de
Craig <craig.coleman.cc@gmail.com>
2022-12-06 12:24:28 -0600

    Validations in course model

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,4 +3,8 @@ class Course < ApplicationRecord
     has_many :assignments, dependent: :destroy  
     has_many :periods 
     has_many :students, through: :periods 
+
+    validates :title, presence: true 
+    validates :period, presence: true
+    validates :grade_level, presence: true, numericality: { in: 7..12, integer_only: true}
 end
