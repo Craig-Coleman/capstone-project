98d06fdcb79624191fcecf617a8f9f1a2a983642
Craig <craig.coleman.cc@gmail.com>
2022-12-07 16:44:08 -0600

    Component files in components folder and imports lines in App file

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Header from './Header';
+import Auth from './Auth';
+import CourseList from './CourseList';
+import CourseHome from './CourseHome';
+import Roster from './Roster';
+import StudentInfo from './StudentInfo';
+import Gradebook from './Gradebook';
+import UserPage from './UserPage';
+import StudentList from './StudentList';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1>App</h1>
   );
 }
 
