394571065d7a970517ad9069261944bbb68bfa50
Craig <craig.coleman.cc@gmail.com>
2022-12-16 15:12:39 -0600

    adjusted serializers so that course fetch is brought with assignments but not students

--- app/serializers/course_serializer.rb
+++ app/serializers/course_serializer.rb
@@ -1,6 +1,5 @@
 class CourseSerializer < ActiveModel::Serializer
   attributes :id, :title, :period, :grade_level
 
-  has_many :students 
   has_many :assignments
 end
--- app/serializers/student_serializer.rb
+++ app/serializers/student_serializer.rb
@@ -1,6 +1,4 @@
 class StudentSerializer < ActiveModel::Serializer
   attributes :id, :first_name, :last_name, :grade_level, :classification, :birth_date
 
-  has_many :courses
-  has_many :assignments
 end
